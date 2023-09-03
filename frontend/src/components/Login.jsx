46cd1f547bca8d4520faf41ab508fc6aa62add51
Kaushik <akaushikarkar@gmail.com>
2023-09-03 12:17:58 +0530

    changed the api url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const response = await fetch("http://localhost:6969/api/auth/login", {
+        const response = await fetch("https://i-notebook-five.vercel.app/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -12,7 +12,7 @@ const SignUp = (props) => {
     e.preventDefault()
 
     const {name, email, password} = credentials
-    const response = await fetch("http://localhost:6969/api/auth/createuser", {
+    const response = await fetch("https://i-notebook-five.vercel.app/api/auth/createuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
