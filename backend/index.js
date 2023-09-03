b92ec2a5c5353b52fcdeeda2342778a1f94d3ec7
Kaushik <akaushikarkar@gmail.com>
2023-09-03 12:09:37 +0530

    added vercel in backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 connectToMongo();
 
 const app = express()
-const port = 6969
+const port = process.env.PORT || 6969;
 
 app.use(cors())
 app.use(express.json())
@@ -16,3 +16,5 @@ app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
   console.log(`iNotebook backend is listening on port ${port}`)
 })
+
+export default app;
